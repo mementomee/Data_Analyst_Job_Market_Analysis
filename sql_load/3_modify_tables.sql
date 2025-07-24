8eca675a597e8afae70887cf74f81146ab2198b4
Nazar Petrashchuk <petrasuknazar@gmail.com>
2025-07-24 07:50:13 +0300

    Додав інструкції для вирішення проблем з доступом до файлів під час завантаження даних з CSV

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -13,30 +13,28 @@ NOTE: If you are having issues with permissions. And you get error:
     1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
 5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
 
-\copy company_dim FROM 'C:\Users\Nazar\Desktop\Analyse\Data Wizard\Luke Barousse\all_folders\csv_files/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy company_dim FROM 'C:\Users\Nazar\Desktop\Project1\csv_files\company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_dim FROM 'C:\Users\Nazar\Desktop\Analyse\Data Wizard\Luke Barousse\all_folders\csv_files/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy skills_dim FROM 'C:\Users\Nazar\Desktop\Project1\csv_files\skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy job_postings_fact FROM 'C:\Users\Nazar\Desktop\Analyse\Data Wizard\Luke Barousse\all_folders\csv_files/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy job_postings_fact FROM 'C:\Users\Nazar\Desktop\Project1\csv_files\job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_job_dim FROM 'C:\Users\Nazar\Desktop\Analyse\Data Wizard\Luke Barousse\all_folders\csv_files/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy skills_job_dim FROM 'C:\Users\Nazar\Desktop\Project1\csv_files\skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 */
 
--- NOTE: This has been updated from the video to fix issues with encoding
-
 COPY company_dim
-FROM 'C:\Users\Nazar\Desktop\Analyse\Data Wizard\Luke Barousse\all_folders\csv_files\company_dim.csv'
+FROM 'C:\Users\Nazar\Desktop\Project1\csv_files\company_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_dim
-FROM 'C:\Users\Nazar\Desktop\Analyse\Data Wizard\Luke Barousse\all_folders\csv_files\skills_dim.csv'
+FROM 'C:\Users\Nazar\Desktop\Project1\csv_files\skills_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY job_postings_fact
-FROM 'C:\Users\Nazar\Desktop\Analyse\Data Wizard\Luke Barousse\all_folders\csv_files\job_postings_fact.csv'
+FROM 'C:\Users\Nazar\Desktop\Project1\csv_files\job_postings_fact.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_job_dim
-FROM 'C:\Users\Nazar\Desktop\Analyse\Data Wizard\Luke Barousse\all_folders\csv_files\skills_job_dim.csv'
+FROM 'C:\Users\Nazar\Desktop\Project1\csv_files\skills_job_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
