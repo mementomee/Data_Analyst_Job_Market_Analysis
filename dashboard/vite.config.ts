c36771e78832406283bde42ec1c8de13b1b5d81a
Nazar Petrashchuk <petrasuknazar@gmail.com>
2025-07-26 18:49:36 +0300

    make git actions

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -3,20 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
+  base: '/назва-вашого-репозиторію/', // Замініть на назву вашого репо
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})
